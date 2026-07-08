aacf4b6ec2b2fd31795f39e032084343d7dbb72a
Jessica Smith <jessasmith2005@gmail.com>
2026-07-08 22:20:10 +0100

    feat: adding scrollllly things

---
+++ data/posts.js
@@ -0,0 +1,20 @@
+export const posts = [
+    {
+      id: 1,
+      name: "Jessica",
+      caption: "Friday drinks 🍻",
+      time: "2 hours ago",
+    },
+    {
+      id: 2,
+      name: "Tom",
+      caption: "Anyone up for a walk this weekend?",
+      time: "Yesterday",
+    },
+    {
+      id: 3,
+      name: "Sarah",
+      caption: "Great picnic yesterday ☀️",
+      time: "3 days ago",
+    },
+  ];
--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,38 +1,35 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { ScrollView, Text, StyleSheet } from 'react-native';
 import PostCard from '../components/PostCard';
+import { posts } from '../data/posts';
 
 export default function FeedScreen() {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.title}>
         Neighbours 📸
       </Text>
 
-      <PostCard
-        name="Jessica"
-        caption="Friday drinks 🍻"
-        time="2 hours ago"
-      />
+      {posts.map((post) => (
+        <PostCard
+          key={post.id}
+          name={post.name}
+          caption={post.caption}
+          time={post.time}
+        />
+      ))}
 
-      <PostCard
-        name="Tom"
-        caption="Anyone up for a walk?"
-        time="Yesterday"
-      />
-
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
     paddingTop: 60,
   },
 
   title: {
     fontSize: 32,
     fontWeight: 'bold',
+    marginBottom: 20,
   },
 });
